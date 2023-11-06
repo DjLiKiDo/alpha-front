0ba330a226004c051a9f1ca2694d760b62b15ca8
DjLiKiDo <djlikido@gmail.com>
2023-11-06 22:30:53 +0100

    HomePage update Styles update

--- src/components/common/NavBar/NavBar.tsx
+++ src/components/common/NavBar/NavBar.tsx
@@ -1,38 +1,76 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import styles from './NavBar.module.css'; // Using CSS module for scoped styling
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styles from "./NavBar.module.css"; // Using CSS module for scoped styling
+import logo from "../../../assets/images/alpha-logo.png"; // Ajusta la ruta según la estructura de tu proyecto
 
 const NavBar = () => {
   return (
     <nav className={styles.navbar}>
       <div className={styles.navbarBrand}>
-        <NavLink to="/" className={(navData) => navData.isActive ? styles.active : ""}>
-          <img src="/assets/logo.svg" alt="Alpha Logo" className={styles.navbarLogo} />
+        <NavLink
+          to="/"
+          className={(navData) =>
+            navData.isActive
+              ? `${styles.navLink} ${styles.active}`
+              : styles.navLink
+          }
+        >
+          <img src={logo} alt="Alpha Logo" className={styles.navbarLogo} />
         </NavLink>
       </div>
       <ul className={styles.navbarNav}>
         <li className={styles.navItem}>
-          <NavLink to="/about" className={(navData) => navData.isActive ? styles.active : ""} >About Us</NavLink>
+          <NavLink
+            to="/about"
+            className={(navData) => (navData.isActive ? styles.active : "")}
+          >
+            About Us
+          </NavLink>
         </li>
         <li className={styles.navItem}>
-          <NavLink to="/services" className={(navData) => navData.isActive ? styles.active : ""} >Services</NavLink>
+          <NavLink
+            to="/services"
+            className={(navData) => (navData.isActive ? styles.active : "")}
+          >
+            Services
+          </NavLink>
         </li>
         <li className={styles.navItem}>
-          <NavLink to="/technology" className={(navData) => navData.isActive ? styles.active : ""} >Technology</NavLink>
+          <NavLink
+            to="/technology"
+            className={(navData) => (navData.isActive ? styles.active : "")}
+          >
+            Technology
+          </NavLink>
         </li>
         <li className={styles.navItem}>
-          <NavLink to="/insights" className={(navData) => navData.isActive ? styles.active : ""} >Insights</NavLink>
+          <NavLink
+            to="/insights"
+            className={(navData) => (navData.isActive ? styles.active : "")}
+          >
+            Insights
+          </NavLink>
         </li>
         <li className={styles.navItem}>
-          <NavLink to="/careers" className={(navData) => navData.isActive ? styles.active : ""} >Careers</NavLink>
+          <NavLink
+            to="/careers"
+            className={(navData) => (navData.isActive ? styles.active : "")}
+          >
+            Careers
+          </NavLink>
         </li>
         <li className={styles.navItem}>
-          <NavLink to="/contact" className={(navData) => navData.isActive ? styles.active : ""} >Contact</NavLink>
+          <NavLink
+            to="/contact"
+            className={(navData) => (navData.isActive ? styles.active : "")}
+          >
+            Contact
+          </NavLink>
         </li>
       </ul>
       {/* Here you could add additional elements like a search or sign in/up buttons */}
     </nav>
   );
-}
+};
 
 export default NavBar;
--- src/views/HomePage/HomePage.tsx
+++ src/views/HomePage/HomePage.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import styles from "./HomePage.module.css";
-import { Button } from "react-bootstrap";
+import heroImage from "../../assets/images/hero-image.png"; // Asegúrate de que la ruta sea correcta
+
 
 const HomePage: React.FC = () => {
   return (
     <div className={styles.homePageContainer}>
-      <div className={styles.mainSection}>
+      <div className={styles.heroSection} style={{ backgroundImage: `url(${heroImage})` }}>
         <h1 className={styles.title}>Alpha Blockchain & AI Solutions</h1>
         <p className={styles.subtitle}>Democratizando el acceso a las tecnologías emergentes.</p>
-        <Button className={styles.mainButton}>Más información</Button>
+        <button className={styles.mainButton}>Más información</button>
       </div>
     </div>
   );
